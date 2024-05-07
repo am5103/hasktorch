22fef43b9186005a5c54ab0a8782f36c2d7fcc5a
acf16407il <koto3fujita@gmail.com>
2024-05-07 16:36:28 +0900

    3.a

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,34 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Control.Monad (when)
 import Torch
-import Text.CSV
+-- import Text.CSV(Text)
+import GHC.Generics(Generic)
+import System.IO
+import System.Exit (exitFailure)
+-- from bytestring
+import Data.ByteString (ByteString, hGetSome, empty)
+import qualified Data.ByteString.Lazy as BL
+-- from cassava
+import Data.Csv.Incremental
+import Data.Csv (FromRecord, ToRecord)
+import Data.Text (Text)
+
+data Tempature = Tempature {
+  date :: !Text,
+  dairy_mean_tempature :: !Float }
+  deriving (Generic,Show)
+
+instance FromRecord Tempature
+instance ToRecord Tempature
 
 model :: Linear -> Tensor -> Tensor
 model state input = squeezeAll $ linear state input
@@ -23,8 +45,24 @@ printParams trained = do
   putStrLn $ "Parameters:\n" ++ (show $ toDependent $ trained.weight)
   putStrLn $ "Bias:\n" ++ (show $ toDependent $ trained.bias)
 
+-- readfile
+feed :: (ByteString -> Parser Tempature) -> Handle -> IO (Parser Tempature)
+feed k csvFile = do
+  hIsEOF csvFile >>= \case
+    True  -> return $ k empty
+    False -> k <$> hGetSome csvFile 4096
+
+readFromFile :: IO ()
+readFromFile = do
+  withFile "data/valid.csv" ReadMode $ \ csvFile -> do
+    let loop !_ (Fail _ errMsg) = do putStrLn errMsg; exitFailure
+        loop acc (Many rs k)    = loop (acc <> rs) =<< feed k csvFile
+        loop acc (Done rs)      = print (acc <> rs)
+    loop [] (decode NoHeader)
+
 main :: IO ()
 main = do
+  readFromFile
   init <- sample $ LinearSpec {in_features = numFeatures, out_features = 1}
   randGen <- defaultRNG
   printParams init
