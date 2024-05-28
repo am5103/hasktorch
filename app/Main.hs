850ad15033aca51e1adf7e9aa5d6d7c27cf7433a
acf16407il <koto3fujita@gmail.com>
2024-05-28 14:55:55 +0900

    3.c?

--- app/Main.hs
+++ app/Main.hs
@@ -38,57 +38,32 @@ instance FromNamedRecord Temprature where
 model :: Linear -> Tensor -> Tensor
 model state input = squeezeAll $ linear state input
 
-groundTruth :: Tensor -> Tensor
-groundTruth t = squeezeAll $ matmul t weight + bias
-  where
-    weight = asTensor ([42.0, 64.0, 96.0] :: [Float])
-    bias = full' [1] (3.14 :: Float)
+-- groundTruth :: Tensor -> Tensor
+-- groundTruth t = squeezeAll $ matmul t weight + bias
+--   where
+--     weight = asTensor ([42.0, 64.0, 96.0] :: [Float])
+--     bias = full' [1] (3.14 :: Float) -- aがweight bがbias(aとbを変数とおく)
+--     -- full :: forall shape dtype device a. (TensorOptions shape dtype device, Scalar a) => a -> Tensor device dtype shape
 
-printParams :: Linear -> IO ()
+printParams :: Linear -> IO () --パラメータを表示
 printParams trained = do
   putStrLn $ "Parameters:\n" ++ (show $ toDependent $ trained.weight)
   putStrLn $ "Bias:\n" ++ (show $ toDependent $ trained.bias)
 
--- readfile
--- feed :: (ByteString -> Parser Temprature) -> Handle -> IO (Parser Temprature)
--- feed k csvFile = do
---   hIsEOF csvFile >>= \case
---     True  -> return $ k empty
---     False -> k <$> hGetSome csvFile 4096
-
-
--- ファイルを読み込んで出力
--- readFromFile :: FilePath -> IO String
--- readFromFile filepath = do
---   withFile filepath ReadMode $ \ csvFile -> do
---     let loop !_ (Fail _ errMsg) = do putStrLn errMsg; exitFailure
---         loop acc (Many rs k)    = loop (acc <> rs) =<< feed k csvFile
---         loop acc (Done rs)      = print (acc <> rs)
-        
---     loop [] (decode NoHeader)
-
---csv_to_array :: ByteString -> 
-
-
--- make_7days_temprature_list [] sevendays_list = []
--- make_7days_temprature_list [x1] sevendays_list = []
--- make_7days_temprature_list [x1,x2] sevendays_list = []
--- make_7days_temprature_list [x1,x2,x3] sevendays_list = []
--- make_7days_temprature_list [x1,x2,x3,x4] sevendays_list = []
--- make_7days_temprature_list [x1,x2,x3,x4,x5] sevendays_list = []
--- make_7days_temprature_list [x1,x2,x3,x4,x5,x6] sevendays_list = []
--- make_7days_temprature_list [x1,x2,x3,x4,x5,x6,x7] sevendays_list = []
--- make_7days_temprature_list [x1,x2,x3,x4,x5,x6,x7,x8] sevendays_list = (sevendays_list ++ [([x1,x2,x3,x4,x5,x6,x7],x8)]) --気温のリストの長さが７の時に終わり
 -- リストの最初の8個のFloatをとってきてその後1~7日のリストと8日目を追加するリストを作成する
-make_7days_temperature_list :: [Float] -> [([Float],Float)] -> [([Float],Float)]
-make_7days_temperature_list temperature_list sevendays_list
+make_7days_temperature_pair_list :: [Float] -> [([Float],Float)] -> [([Float],Float)]
+make_7days_temperature_pair_list temperature_list sevendays_list
   | length temperature_list < 8 = sevendays_list
-  | otherwise = make_7days_temperature_list (Prelude.tail temperature_list) (sevendays_list ++ [(Prelude.take 7 temperature_list, last (Prelude.take 8 temperature_list))])-- 　temprature_listの最初から7個をsevendats_listに追加する
+  | otherwise = make_7days_temperature_pair_list (Prelude.tail temperature_list) (sevendays_list ++ [(Prelude.take 7 temperature_list, last (Prelude.take 8 temperature_list))])-- 　temprature_listの最初から7個をsevendats_listに追加する
 
 
 -- Temprature型を受け取ったらdaily_mean_tempratureを返す      
 return_daily_mean_temprature :: Temprature -> Float
 return_daily_mean_temprature = daily_mean_temprature
+
+-- floatのリストとfloatのペアを受け取ったらfloatのりすとをを返す
+pair_to_list :: ([Float],Float) -> [Float]
+pair_to_list (list_,_) = list_
 
 -- Vector Tempratureを受け取ったらfloatのリストを返す
 -- toList :: Vector a -> [a]
@@ -108,28 +83,37 @@ main = do
         Left err -> []
         Right (_, v) -> make_float_list v
   -- 7日間の気温のリストのリスト
-  let train_sevendays_temprature_list = make_7days_temperature_list train_temprature_list []
-  print train_sevendays_temprature_list
+  let train_sevendays_temprature_pair_list = make_7days_temperature_pair_list train_temprature_list []
+  let train_sevendays_temprature_list = map pair_to_list train_sevendays_temprature_pair_list
+  -- print train_sevendays_temprature_list
   -- valid <- readFile("data/valid.csv")
   -- readFromFile ("data/train.csv")
   -- eval <- readFile("data/eval.csv")
+
+
   init <- sample $ LinearSpec {in_features = numFeatures, out_features = 1} -- sample parametrised classという型クラス　linear specモデルの設定を定義しているデータ型 
   -- infeatures 入ってくる次元を指定　
   randGen <- defaultRNG
   printParams init
   (trained, _) <- foldLoop (init, randGen) numIters $ \(state, randGen) i -> do
-    let (input, randGen') = randn' [batchSize, numFeatures] randGen
-        (y, y') = (groundTruth input, model state input)
-        loss = mseLoss y y'
-    when (i `mod` 100 == 0) $ do
-      putStrLn $ "Iteration: " ++ show i ++ " | Loss: " ++ show loss
-    (newParam, _) <- runStep state optimizer loss 5e-3
-    pure (newParam, randGen')
+    -- let (input, randGen') = randn' [batchSize, numFeatures] randGen --変数を減らす　input:バッチサイズ*特徴量の配列
+    let startIdx = i * batchSize
+        endIdx = startIdx + batchSize
+        input_list =  Prelude.take (endIdx - startIdx) (drop startIdx train_sevendays_temprature_pair_list)--　バッチサイズ分もらってくる 
+        (input_float, y_float) = unzip input_list
+        input = asTensor input_float
+        y = asTensor y_float
+        y' = model state input -- なってほしい値と出た値(8日目の気温)
+        loss = mseLoss y y' --mseの誤差
+    when (i `mod` 10 == 0) $ do
+      putStrLn $ "Iteration: " ++ show i ++ " | Loss: " ++ show loss --　１００回ごとに誤差を表示
+    (newParam, _) <- runStep state optimizer loss 1e-3 -- 更新してくれる関数
+    pure (newParam, randGen) 
   printParams trained
   pure ()
   where
-    optimizer = GD
+    optimizer = GD --勾配降下法
     defaultRNG = mkGenerator (Device CPU 0) 31415
-    batchSize = 4
-    numIters = 2000
-    numFeatures = 3
+    batchSize = 64
+    numIters = 40
+    numFeatures = 7 -- optimiser で暗点に陥らないように、やり方を考える今回は勾配
